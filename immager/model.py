6a227210b6447a1808472f8dfb819f587f80fc47
aakash aman <aakash.aman@student.sl.on.ca>
2024-01-18 10:57:59 -0500

    update1.0.0

--- immager/model.py
+++ immager/model.py
@@ -7,5 +7,3 @@ pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch
 prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
 
 image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
-image = image.to(dtype=torch.float32)
-
